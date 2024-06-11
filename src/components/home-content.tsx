d772e27931da5b06165d501c6959989172e4d5dd
Fabio Henrique <fabiosilva5210@gmail.com>
2024-06-11 00:18:38 -0300

    home traduzida por completo para ingles e espanhol

--- src/components/home-content.tsx
+++ src/components/home-content.tsx
@@ -1,10 +1,15 @@
+import { useTranslation } from "react-i18next"
+
+
 function HomeContent() {
+    const { t } = useTranslation()
+
     return (
         <>
             <main>
                     <div>
-                        <h2 className="text-[28px] font-[500] m-5">Recomendações do dia</h2>
-                        <p className="font-[#313131] text-[18px] ml-5">Conheça uma nova ONG</p>
+                        <h2 className="text-[28px] font-[500] m-5">{t('recomendacoesDia')}</h2>
+                        <p className="font-[#313131] text-[18px] ml-5">{t('conhecaOng')}</p>
 
 
                         <div className="flex m-5 mb-28 justify-between">
@@ -15,28 +20,28 @@ function HomeContent() {
                     </div>
 
                 <div className="m-5">
-                    <h2 className="m-5 font-[600] text-[28px]">SUA VAGA IDEAL</h2>
+                    <h2 className="m-5 font-[600] text-[28px]">{t('vagaIdeal')}</h2>
 
                     <div className="flex justify-between">
                         <section className="shadow-xl bg-[#DAF1FFAD] max-w-[400px] rounded-[24px] p-5">
                             <div className="flex items-center justify-center">
                                 <div className="flex items-center space-x-4">
                                     <div>
-                                        <h2 className="text-[28px] font-semibold mr-14">Título da vaga</h2>
-                                        <p className="text-base text-[18px] mb-2">Local | Hora</p>
+                                        <h2 className="text-[28px] font-semibold mr-14">{t('tituloVaga')}</h2>
+                                        <p className="text-base text-[18px] mb-2">{t('informacoes')}</p>
                                     </div>
                                     <img src="https://via.placeholder.com/75" alt="Logo da ONG" className="object-cover rounded-[10px]" />
                                 </div>
                             </div>
 
-                            <p className="font-[500] pl-1">Requisitos Necessários:</p>
+                            <p className="font-[500] pl-1">{t('requisitos')}</p>
                             <ul className=" text-[18px] font-[500] list-disc p-7">
-                                <li>Requisito 1</li>
-                                <li>Requisito 2</li>
-                                <li>Requisito 3</li>
+                                <li>{t('requisitoa')}</li>
+                                <li>{t('requisitoa')}</li>
+                                <li>{t('requisitoa')}</li>
                             </ul>
                             <div className="text-center">
-                                <button className="bg-[#004AAD] text-white font-[600] py-2 px-4 rounded-[10px]">ME CANDIDATAR</button>
+                                <button className="bg-[#004AAD] text-white font-[600] py-2 px-4 rounded-[10px]">{t('candidatar')}</button>
                             </div>
                         </section>
 
@@ -44,21 +49,21 @@ function HomeContent() {
                             <div className="flex items-center justify-center">
                                 <div className="flex items-center space-x-4">
                                     <div>
-                                        <h2 className="text-[28px] font-semibold mr-14">Título da vaga</h2>
-                                        <p className="text-base text-[18px] mb-2">Local | Hora</p>
+                                        <h2 className="text-[28px] font-semibold mr-14">{t('tituloVaga')}</h2>
+                                        <p className="text-base text-[18px] mb-2">{t('informacoes')}</p>
                                     </div>
                                     <img src="https://via.placeholder.com/75" alt="Logo da ONG" className="object-cover rounded-[10px]" />
                                 </div>
                             </div>
 
-                            <p className="font-[500] pl-1">Requisitos Necessários:</p>
+                            <p className="font-[500] pl-1">{t('requisitos')}</p>
                             <ul className=" text-[18px] font-[500] list-disc p-7">
-                                <li>Requisito 1</li>
-                                <li>Requisito 2</li>
-                                <li>Requisito 3</li>
+                                <li>{t('requisitoa')}</li>
+                                <li>{t('requisitoa')}</li>
+                                <li>{t('requisitoa')}</li>
                             </ul>
                             <div className="text-center">
-                                <button className="bg-[#004AAD] text-white font-[600] py-2 px-4 rounded-[10px]">ME CANDIDATAR</button>
+                                <button className="bg-[#004AAD] text-white font-[600] py-2 px-4 rounded-[10px]">{t('candidatar')}</button>
                             </div>
                         </section>
 
@@ -66,21 +71,21 @@ function HomeContent() {
                             <div className="flex items-center justify-center">
                                 <div className="flex items-center space-x-4">
                                     <div>
-                                        <h2 className="text-[28px] font-semibold mr-14">Título da vaga</h2>
-                                        <p className="text-base text-[18px] mb-2">Local | Hora</p>
+                                        <h2 className="text-[28px] font-semibold mr-14">{t('tituloVaga')}</h2>
+                                        <p className="text-base text-[18px] mb-2">{t('informacoes')}</p>
                                     </div>
                                     <img src="https://via.placeholder.com/75" alt="Logo da ONG" className="object-cover rounded-[10px]" />
                                 </div>
                             </div>
 
-                            <p className="font-[500] pl-1">Requisitos Necessários:</p>
+                            <p className="font-[500] pl-1">{t('requisitos')}</p>
                             <ul className=" text-[18px] font-[500] list-disc p-7">
-                                <li>Requisito 1</li>
-                                <li>Requisito 2</li>
-                                <li>Requisito 3</li>
+                                <li>{t('requisitoa')}</li>
+                                <li>{t('requisitoa')}</li>
+                                <li>{t('requisitoa')}</li>
                             </ul>
                             <div className="text-center">
-                                <button className="bg-[#004AAD] text-white font-[600] py-2 px-4 rounded-[10px]">ME CANDIDATAR</button>
+                                <button className="bg-[#004AAD] text-white font-[600] py-2 px-4 rounded-[10px]">{t('candidatar')}</button>
                             </div>
                         </section>
                     </div>
