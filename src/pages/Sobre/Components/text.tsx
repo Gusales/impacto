86e1dc556cce8263a3a9dfa41bd41ed5a7845ed5
Gabriel Braz <gabrielphc10@gmail.com>
2024-06-22 21:43:14 -0300

    Inicio da Pagina Sobre

---
+++ src/pages/Sobre/Components/text.tsx
@@ -0,0 +1,9 @@
+export function Text(){
+    return(
+        <>
+        <div>
+            <h1>teste</h1>
+        </div>
+        </>
+    )
+}
---
+++ src/pages/Sobre/sobre.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/components-impacto/header/header"
+import { Text } from "@/pages/Sobre/Components/text"
+import Footer from "@/components/components-impacto/footer"
+
+export function Sobre() {
+    return(
+        <>
+            <Header />
+            <Text />
+            <Footer />
+        </>
+    )
+}
