82149d66d6891c519801937abb27e14a37df2364
Gusales <dev.gussales@gmail.com>
2024-06-24 10:32:37 -0300

    arrumando detalhes na tela de sobre
    Neste commit foi realizado um ajuste de espaçamento na seção de missão da tela sobre

--- src/pages/Sobre/Components/content.tsx
+++ src/pages/Sobre/Components/content.tsx
@@ -8,7 +8,7 @@ interface ContentProps{
 
 export function Content({ content, imgAlt, imgPosition = "right", imgSrc, title }: ContentProps){
     return(
-        <article className={`flex w-fit items-center mb-12 flex-col-reverse md:flex-row ${imgPosition === "left" && "md:flex-row-reverse"}`}>
+        <article className={`flex w-fit items-center mb-12 flex-col-reverse md:flex-row ${imgPosition === "left" && "gap-10 md:flex-row-reverse"}`}>
             <div className="w-full md:w-3/4 flex flex-col gap-4 mt-4 md:mt-0 items-center md:items-start">
                 <h2 className="border-b-4 border-[#004AAD] w-fit text-2xl">{title}</h2>
                 <p className={`${imgPosition === "right" && "w-full md:w-2/3"}`}>{content}</p>
