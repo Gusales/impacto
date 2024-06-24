5a7182394b28119167b0a0b6a1feecd82d365c16
Gusales <dev.gussales@gmail.com>
2024-06-23 22:23:22 -0300

    ajuste na responsividade do componente parceiro

--- src/pages/Sobre/Components/parceiro.tsx
+++ src/pages/Sobre/Components/parceiro.tsx
@@ -10,7 +10,7 @@ export function Parceiro({ cover, coverAlt, nome }: ParceiroProps){
       <div className="overflow-hidden rounded-full">
         <img src={cover} alt={coverAlt} className="w-48 rounded-full hover:scale-105 transition-all" />
       </div>
-      <h4 className="text-xl font-semibold text-[#004358] mt-4">{nome}</h4>
+      <h4 className="text-lg md:text-xl font-semibold text-[#004358] mt-4">{nome}</h4>
       <a href="" target="_blank" className="hover:underline hover:underline-offset-2 w-fit block">Saiba mais</a>
     </article>
   )
