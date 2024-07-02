3fdbdfb0610413ed407159f70042ea21098e79e5
Gusales <dev.gussales@gmail.com>
2024-07-02 12:18:31 -0300

    tradução da tela de pontos

--- src/pages/Shopping/shopping.tsx
+++ src/pages/Shopping/shopping.tsx
@@ -6,7 +6,8 @@ import { Hero } from "./Components/hero";
 import PlaceholderXbox from '@/assets/images-shopping/placeholder-card-example.png'
 import PlaceholderIfood from '@/assets/images-shopping/placegolder-ifood.png'
 import PlaceholderMcDonalds from '@/assets/images-shopping/mcdonalds-brazil.webp'
-import { vounchersAPI } from "@/lib/axios";
+import { api, vounchersAPI } from "@/lib/axios";
+import { useTranslation } from "react-i18next";
 
 interface Vounchers {
     id: number,
@@ -17,12 +18,19 @@ interface Vounchers {
     pontos: number
 }
 
+interface User{
+  nome: string
+}
+
 export function ShoppingPage(){
   document.title = "Resgate seus pontos || Impacto"
   const [vounchers, setVounchers] = useState<Vounchers[] | []>([])
+  const [user, setUser] = useState<User | string>('')
   const [erro, setErro] = useState('')
 
-  useEffect(() => {
+  const { t } = useTranslation()
+
+  async function getVounchers(){
     vounchersAPI.get('/')
     .then(({ data: { vounchers } }) => {
       setVounchers(vounchers)
@@ -33,26 +41,50 @@ export function ShoppingPage(){
       if (code === "ERR_NETWORK") {
         setErro('Não foi possível carregar os vounchers e cupons de desconto')
       }
-      
     })
+  }
+
+  const getUserId = localStorage.getItem('userId') ? localStorage.getItem('userId') : ''
+
+  async function getUser(){
+    api.get(`/api/usuarios/${getUserId}`)
+    .then(({data}) => {
+      console.log(data)
+      setUser(data.nome)
+    })
+  }
+
+  useEffect(() => {
+    Promise.all([getVounchers(), getUser()])
   },[])
 
   return (
     <div className="flex flex-col items-center">
       <div className="w-[80%] mb-6">
         <h1 className="text-2xl font-semibold">
-          Já fez uma doação hoje?
+          {t('shoppingFezUmaDoacaoHoje')}
         </h1>
         <h2 className="text-lg font-medium">
-          Queremos te recompensar por isso!
+          {t('shoppingRecompensarPorIsso')}
         </h2>
 
         <div className="flex flex-col items-center lg:grid grid-cols-2 lg:space-x-4">
-          <Hero buttonText="Confira" content="Aumete seus pontos fazendo doações e trabalhos voluntários!" title={`Olá${``}`} />
-          <Hero buttonText="Convidar" content="Convite um amigo para doar em qualquer ong e ganhe 10 pontos!" title="Convide e ganhe!" />
+          <Hero 
+            buttonText={t('shoppingConvideAgora')} 
+            content={t('shoppingAumentarPontosDoacao')}
+            title={`${t('shoppingOi')}${user !== '' && ', ' + user || ''}!`} 
+          />
+
+          <Hero 
+            buttonText={t('shoppingConfira')} 
+            content={t('shoppingAumentarPontosConvidando')}
+            title={t('shoppingConvide')} 
+          />
         </div>
       
-        <h2 className="mt-8 text-2xl font-medium">Troque seus pontos!</h2>
+        <h2 className="mt-8 text-2xl font-medium">
+          { t('shoppingTroqueSeusPontos') }
+        </h2>
 
         <div className="flex gap-4 mt-5">
           <button className="bg-[#004AAD] text-white py-1 w-[100px] text-sm hover:opacity-85 transition-all rounded-full">gift-cards</button>
@@ -61,10 +93,12 @@ export function ShoppingPage(){
 
         <section className="mt-8 space-y-5">
           <div className="flex flex-col sm:flex-row sm:items-center sm:gap-0 gap-2 justify-between">
-            <h3 className="text-2xl font-medium">Recompensas sugeridas</h3>
+            <h3 className="text-2xl font-medium">
+              { t('shoppingRecompensasSugeridas') }
+            </h3>
 
             <a href="" className="flex items-center gap-1 text-[#15406B] font-bold hover:underline hover:underline-offset-2">
-              Ver todas as recompensas
+              {t('shoppingVerTodasAsRecompensas')}
               <ChevronRight />
             </a>
           </div>
